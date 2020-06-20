7e86ccd882b6e13e5f39d8c40dea3586c633d904
unknown <damodar2dikonda@gmail.com>
2020-06-20 15:18:25 +0530

    Javascript progra,m

--- javas/reg_email.py
+++ javas/reg_email.py
@@ -18,7 +18,7 @@
             var reg=/^([a-zA-Z0-9\.-]+)@([a-zA-z0-9-]+).([a-zA-z]{2,20}).([a-zA-Z]{2,10})?$/    //^=starting and $=ending and abcd is invalid
             // + min one or more than 1
         //? entire is optional
-          // {2,10}min=2 and max=10
+          // {2,10}min=2 and max=10;
             //var reg=/abc/   abcd is valid
             if(reg.test(text)){
 
